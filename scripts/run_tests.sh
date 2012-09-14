f884f6ee6fd70c14c74768bcfcbc389000687384
Attila M. Magyar <attila.m.magyar@gmail.com>
2012-09-15 00:13:04 +0200

    vimrc: running unit tests in a more friendly way
    Opening output of tests in case of failure in a new window.

---
+++ scripts/run_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+run_tests()
+{
+    if [ -f "Makefile" -o -f "makefile" ]
+    then
+        bake check
+        return $?
+    elif [ -f "build.xml" ]
+    then
+        ant test || ant junit
+        return $?
+    fi
+    echo "Where's a makefile or a build.xml when you need it? :-("
+    return 0
+}
+
+output_file="$1"
+if [ "$output_file" = "" ]
+then
+    output_file="/dev/null"
+fi
+
+cd "`git rev-parse --show-cdup`" 2>&1 >/dev/null
+run_tests 2>&1 | tee "$output_file"
+exit_code=${PIPESTATUS[0]}
+sleep 1.2
+exit $exit_code
