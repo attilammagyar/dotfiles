2d0a6e16704b714d980eaf5e1b4c2a201886d34b
Attila M. Magyar <attila.m.magyar@gmail.com>
2013-04-06 16:59:16 +0200

    vimrc: make git commits right from the editor

--- scripts/vim_git.sh
+++ scripts/vim_git.sh
@@ -15,6 +15,8 @@ function main()
         grep) git_grep "$@" ;;
         log) git_log "$base_name" "$@" ;;
         blame) git_blame "$base_name" "$@" ;;
+        commit_all) git_commit_all ;;
+        commit_selection) git_commit_selection ;;
         *) retval=1 ;;
     esac
     cd -
@@ -26,16 +28,21 @@ function git_grep()
 {
     word="$1"
 
-    cd "`git rev-parse --show-cdup`"
+    git_cd_to_root
     git grep -C3 "$word" \
         | less -r
+}
+
+function git_cd_to_root()
+{
+    cd "`git rev-parse --show-cdup`"
 }
 
 function git_log()
 {
     source_file="$1"
 
-    git log --patch-with-stat --follow "$source_file" \
+    git log --decorate --patch-with-stat --follow "$source_file" \
         | less -r
 }
 
@@ -45,6 +52,20 @@ function git_blame()
     line="$2"
 
     git blame -L "$line,+1" "$source_file"
+}
+
+function git_commit_all()
+{
+    git_cd_to_root
+    git add .
+    git commit -a --verbose
+}
+
+function git_commit_selection()
+{
+    git_cd_to_root
+    git add -p .
+    git commit --verbose
 }
 
 main "$@"
