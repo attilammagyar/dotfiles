8750924b18a0068e928bd6024ebcc07458a14b34
Attila M. Magyar <attila.m.magyar@gmail.com>
2022-01-30 12:52:16 +0100

    loudness.sh: produce TSV output

--- scripts/loudness.sh
+++ scripts/loudness.sh
@@ -5,20 +5,23 @@ main()
     local file
 
     cat <<HELP
-  LUFS = Loudness Unit Full Scale (Loudness K-weighted relative to full scale)
-  LU   = Relative Loudness Unit (1 LU ~ relative measurement of 1 dB)
-
-  I    = Integrated loudness (average loudness over the whole program)
-  IT   = Integrated loudness threshold
-  LRA  = Loudness range (LH - LL)
-  LT   = Loudness range threshold (20 LU below Absolute-Gated Loudness which is
-         the avg of short-term loudness values that exceed -70 LUFS with 3s
-         sliding window)
-  LL   = Loudness range low (quietest 10% above LT)
-  LH   = Loudness range high (loudest 95% above LT)
-  P    = True peak (inter-sample peaks)
-
+# LUFS = Loudness Unit Full Scale (Loudness K-weighted, relative to full scale)
+# LU   = Relative Loudness Unit (1 LU ~ relative measurement of 1 dB)
+#
+# I    = Integrated loudness (average loudness over the whole program)
+# IT   = Integrated loudness threshold
+# LRA  = Loudness range (LH - LL)
+# LT   = Loudness range threshold (20 LU below Absolute-Gated Loudness which is
+#        the avg of short-term loudness values that exceed -70 LUFS, with 3s
+#        sliding window)
+# LL   = Loudness range low (quietest 10% above LT)
+# LH   = Loudness range high (loudest 95% above LT)
+# P    = True peak (inter-sample peaks)
+#
 HELP
+
+    printf "I\tIT\tLRA\tLT\tLL\tLH\tP\tFile\n"
+    printf "(LUFS)\t(LUFS)\t(LU)\t(LUFS)\t(LUFS)\t(LUFS)\t(dBFS)\t\n"
 
     while [[ $# -gt 0 ]]
     do
@@ -48,15 +51,15 @@ print_summary()
             -f null - 2>&1 \
             | tail -n 12)
 
-    integrated=$(extract_from_summary "I" "$summary")
-    i_threshold=$(extract_from_summary "Threshold" "$summary" | head -n1)
-    lra=$(extract_from_summary "LRA" "$summary")
-    lra_threshold=$(extract_from_summary "Threshold" "$summary" | tail -n1)
-    lra_low=$(extract_from_summary "LRA low" "$summary")
-    lra_high=$(extract_from_summary "LRA high" "$summary")
-    peak=$(extract_from_summary "Peak" "$summary")
+    integrated=$(extract_from_summary "I" "$summary" "LUFS")
+    i_threshold=$(extract_from_summary "Threshold" "$summary" "LUFS" | head -n1)
+    lra=$(extract_from_summary "LRA" "$summary" "LU")
+    lra_threshold=$(extract_from_summary "Threshold" "$summary" "LUFS" | tail -n1)
+    lra_low=$(extract_from_summary "LRA low" "$summary" "LUFS")
+    lra_high=$(extract_from_summary "LRA high" "$summary" "LUFS")
+    peak=$(extract_from_summary "Peak" "$summary" "dBFS")
 
-    printf "I: %s\tIT: %s\tLRA: %s\tLT: %s\tLL: %s\tLH: %s\tP: %s\t%s\n" \
+    printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
         "$integrated" "$i_threshold" \
         "$lra" "${lra_threshold}" "${lra_low}" "${lra_high}" \
         "$peak" "$file"
@@ -68,11 +71,12 @@ extract_from_summary()
 {
     local field="$1"
     local summary="$2"
+    local unit="$3"
 
     printf "%s" "$summary" \
         | grep "^ *$field: " \
         | cut -d":" -f2 \
-        | sed "s/^ *//"
+        | sed "s/^ *// ; s/ $unit//"
 }
 
 main "$@"
